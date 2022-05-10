d1a73f30430f17eee92dc242f9ccbc8ec67f30c3
geralo17 <79506777+geralo17@users.noreply.github.com>
2022-05-11 00:16:10 +1000

    added alpha beta pruning

--- BranchOrder/player.py
+++ BranchOrder/player.py
@@ -31,7 +31,7 @@ class Player:
         for row in range(n):
             for column in range(n):
                 # Value represents eval function
-                self.possibleMoves[(row, column)] = None
+                self.possibleMoves[(row, column)] = 0
 
     def action(self):
         """
@@ -52,22 +52,22 @@ class Player:
         else:
             chosen = self.minimaxDecision()
         return ("PLACE", chosen[0], chosen[1])
-    
+
     def turn(self, player, action):
         """
-        Called at the end of each player's turn to inform this player of 
-        their chosen action. Update your internal representation of the 
-        game state based on this. The parameter action is the chosen 
-        action itself. 
-        
+        Called at the end of each player's turn to inform this player of
+        their chosen action. Update your internal representation of the
+        game state based on this. The parameter action is the chosen
+        action itself.
+
         Note: At the end of your player's turn, the action parameter is
         the same as what your player returned from the action method
         above. However, the referee has validated it at this point.
-        """ 
+        """
 
         if self.player == player:
             if action[0] == 'STEAL':
-                self.possibleMoves[self.opponentMove] = None
+                self.possibleMoves[self.opponentMove] = 0
                 self.opponentTaken.remove(self.opponentMove)
                 invertedHex = self.invert(self.opponentMove)
                 self.lastMove = invertedHex
@@ -80,7 +80,7 @@ class Player:
                 # Add capture hexes to possibleMoves
                 for hex in self.capture(self.lastMove, player, self.hexTaken, self.opponentTaken):
                     self.opponentTaken.remove(hex)
-                    self.possibleMoves[hex] = None
+                    self.possibleMoves[hex] = 0
             self.numTurns += 1
         else:
             if action[0] == "STEAL":
@@ -96,19 +96,21 @@ class Player:
                 self.opponentTaken.append(self.opponentMove)
                 for hex in self.capture(self.opponentMove, player, self.hexTaken, self.opponentTaken):
                     self.hexTaken.remove(hex)
-                    self.possibleMoves[hex] = None
+                    self.possibleMoves[hex] = 0
 
     def minimaxDecision(self):
         """
         Returns the best hex to place based on Minimax
         """
+
         # Update evalScores in possibleMoves
         for hex in self.possibleMoves:
             # Copy the current state
             # newState = [hexTaken, opponentTaken, possibleMoves]
             state = [self.hexTaken, self.opponentTaken, self.possibleMoves]
             # State is represented as hexTaken and opponentTaken
-            self.possibleMoves[hex] = self.minimaxValue(self.applyHex(state, hex, True), Player.CUTOFF_DEPTH, True, -inf, inf)
+            self.possibleMoves[hex] = self.minimaxValue(self.applyHex(state, hex, True), Player.CUTOFF_DEPTH, True,
+                                                        -inf, inf)
         return max(self.possibleMoves, key=self.possibleMoves.get)
 
     def applyHex(self, state, hex, isMax):
@@ -116,6 +118,10 @@ class Player:
         Returns a state represented by copies of hexTaken, opponentTaken and possibleMoves
         """
         newState = [state[0].copy(), state[1].copy(), state[2].copy()]
+
+        print("ORIGINAL:")
+        for i in newState[2]:
+            print('old Possible move: ', i, newState[2][i])
 
         # Add changes to the state
         if self.player == Player.FIRST_PLAYER:
@@ -128,21 +134,27 @@ class Player:
             # Add capture hexes to possibleMoves
             for coordinates in self.capture(hex, player, newState[0], newState[1]):
                 newState[1].remove(coordinates)
-                newState[2][coordinates] = None
+                newState[2][coordinates] = 0
         else:
             newState[1].append(hex)
             for coordinates in self.capture(hex, player, newState[0], newState[1]):
                 newState[0].remove(coordinates)
-                newState[2][coordinates] = None
+                newState[2][coordinates] = 0
         newState[2].pop(hex)
 
+        print("NEW:")
+        for i in newState[2]:
+            print('new Possible move: ', i)
+
         return newState
-        
+
     def minimaxValue(self, state, cutoff, isMax, alpha, beta):
         if cutoff == 0:
             return self.evalFunction(state[0], state[1])
         if isMax:
             best = -inf
+            if None not in state[2].values():
+                state[2] = sorted(state[2].items(), key=lambda x: x[1], reverse=True)
             for hex in state[2]:
                 newState = self.applyHex(state, hex, isMax)
                 best = max(best, self.minimaxValue(newState, cutoff - 1, not isMax, alpha, beta))
@@ -170,8 +182,9 @@ class Player:
         """
         Returns a list of hexes to be removed as a result of capturing
         """
+
         removeHex = set()
-        axialCentre = (coordinate[0], coordinate[1], -coordinate[0] - coordinate[1])
+        axialCentre = (coordinate[0] + 0, coordinate[1] + 0, -coordinate[0] - coordinate[1] + 0)
         for comb in permutations([-1, 0, 1], 2):
             pos = (coordinate[0] + comb[0], coordinate[1] + comb[1])
 
@@ -232,7 +245,7 @@ class Player:
         return removeHex
 
     def hexInBoard(self, hex):
-        return (0 <= hex[0] < self.n and 0<= hex[1] < self.n)
+        return (0 <= hex[0] < self.n and 0 <= hex[1] < self.n)
 
     def inOpponentHex(self, hex):
         return not (hex in self.hexTaken) or not (hex in self.possibleMoves) and self.hexInBoard(hex)
@@ -247,8 +260,7 @@ class Player:
         PLACED_WEIGHT = 0.2
         BLOCKING_WEIGHT = 0.2
         return (COL_WEIGHT * self.heuristic1(hexTaken)) + (ROW_WEIGHT * self.heuristic2(hexTaken)) + \
-               (CAPTURE_WEIGHT * self.captureHeuristic(opponentTaken)) + (BLOCKING_WEIGHT * \
-                self.blockingEvaluation(hexTaken, opponentTaken)) + (PLACED_WEIGHT * self.placedEvaluation(hexTaken))
+               (CAPTURE_WEIGHT * self.captureHeuristic(opponentTaken)) + (BLOCKING_WEIGHT * self.blockingEvaluation(hexTaken,opponentTaken)) + (PLACED_WEIGHT * self.placedEvaluation(hexTaken))
 
     def heuristic1(self, hexTaken):
         """
@@ -313,12 +325,12 @@ class Player:
             mostFreq = max(freqTable, key=freqTable.get)
             for hex in hexTaken:
                 if hex[0] == mostFreq:
-                    teamCount+=1
+                    teamCount += 1
         else:
             for hex in opponentTaken:
                 freqTable[hex[1]] += 1
             mostFreq = max(freqTable, key=freqTable.get)
             for hex in hexTaken:
                 if hex[1] == mostFreq:
-                    teamCount+=1
+                    teamCount += 1
         return teamCount / freqTable[mostFreq]
